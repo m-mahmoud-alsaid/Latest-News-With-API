6cc77f688d9a4c0f6dbd88e9d6edc266cc7f6ca7
Mohamed Mahmoud <m.mahmoud.alsaid.official@gmail.com>
2026-04-24 15:23:52 +0300

    Finish Sources Cards And Fetch News Sources From Real API

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let wrapper = document.getElementById('wrapper');
+let pageTitle = document.querySelector('.home-page .content h1');
 
 async function fetchHeadlines() {
     try {
@@ -38,7 +39,6 @@ function clearWrapper() {
 }
 
 function headlineCard(headlines) {
-    console.log(headlines.articles);
     headlines.articles.forEach(value => {
         // Card Structure 
         let newDiv = document.createElement('div');
@@ -82,9 +82,47 @@ function headlineCard(headlines) {
     });
 }
 
+function sourcesCard(sources) {
+    console.log(sources.sources);
+
+    for (let source of sources.sources) {
+        let sourcesDiv = document.createElement('div');
+        sourcesDiv.classList.add('sources');
+
+        let sourcesH3 = document.createElement('h3');
+        let sourceH3Text = document.createTextNode(source.name);
+        sourcesH3.appendChild(sourceH3Text);
+
+        let sourcesP = document.createElement('p');
+        let sourcePText = document.createTextNode(source.description);
+        sourcesP.appendChild(sourcePText);
+
+        let sourcesFooter = document.createElement('div');
+        sourcesFooter.classList.add('sources-footer');
+
+        let sourcesFooterP = document.createElement('p');
+        let sourcesFooterPText = document.createTextNode(source.category);
+        sourcesFooterP.appendChild(sourcesFooterPText);
+
+        let sourcesFooterA = document.createElement('a');
+        sourcesFooterA.href = source.url;
+        sourcesFooterA.target = '_blank';
+        sourcesFooterA.innerHTML = 'go to link';
+
+        sourcesFooter.appendChild(sourcesFooterP);
+        sourcesFooter.appendChild(sourcesFooterA);
+
+        sourcesDiv.appendChild(sourcesH3);
+        sourcesDiv.appendChild(sourcesP);
+        sourcesDiv.appendChild(sourcesFooter);
+
+        wrapper.appendChild(sourcesDiv);
+    }
+}
+
 document.querySelector('.side-bar').onclick = (e) => {
-    if (e.target.tagName !== 'LI') return;
-    let li = document.querySelectorAll(".side-bar li");
+    if (e.target.tagName !== 'A') return;
+    let li = document.querySelectorAll(".side-bar a");
     li.forEach((value) => {
         if (value.classList.contains('active')) value.classList.remove('active');
     });
@@ -92,14 +130,19 @@ document.querySelector('.side-bar').onclick = (e) => {
 
     switch (e.target.dataset.view) {
         case 'headlines':
+            pageTitle.innerHTML = 'top news';
             clearWrapper();
             fetchHeadlines().then(data => headlineCard(data));
             break;
         case 'sources':
-            console.log('sources');
+            pageTitle.innerHTML = 'sources';
+            clearWrapper();
+            fetchSources().then(data => sourcesCard(data));
             break;
         case 'categories':
-            console.log('categories');
+            pageTitle.innerHTML = 'categories';
+            clearWrapper();
+            fetchCategories().then(data => console.log(data));
             break;
     }
 };
